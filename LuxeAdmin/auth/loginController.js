d633abeb47135797d32ea3fbf686b3a83d5e7648
Alastair Paragas <alastairparagas@gmail.com>
2015-08-21 12:22:52 -0400

    Login Controller implementation

--- LuxeAdmin/auth/loginController.js
+++ LuxeAdmin/auth/loginController.js
@@ -7,47 +7,69 @@
 "use strict";
 
 var lodash = require('lodash'), 
+    async = require('async'),
     
     authToken = require('./authToken'), 
+    cryptoHash = require('../utility/cryptoHash'),
     apiView = require('../apiView'),
     userModel = require('../datastore/userModel');
 
 function loginController(req, res) {
-
-    userModel
-        .findOne({username: req.body.useridentifier}).exec()
-        .then(function (userDoc) {
-            if (userDoc) {
-                return userDoc;
-            }
+    
+    var userIdentifier = req.body.useridentifier,
+        userPassword = req.body.userpassword,
         
-            return userModel.findOne({email: req.body.useridentifier}).exec();
-        })
-        .then(function (userDoc) {
+        errorMessage = "That username/password combination " +  
+                                "does not exist";
+
+    async.waterfall([
+        function (callback) {
+            userModel.findOne({username: userIdentifier}, callback);
+        },
+        function (userDoc, callback) {
             if (!userDoc) {
-                throw new Error("That username/password combination " +  
-                                "does not exist");   
+                return callback(new Error(errorMessage));
             }
             
-            var userInfo = userDoc.toObject(), 
-                tokenPayload = lodash.pick(userInfo, ['_id', 
-                                                      'username', 
-                                                      'permissions', 
-                                                      'email']);
-            apiView(res, {
-                status: 200,
-                message: "Succesfully authenticated!",
-                data: lodash.merge({
-                    authToken: authToken.generate(tokenPayload)
-                }, userInfo)
-            });
-        })
-        .catch(function (error) {
-            apiView(res, {
+            callback(null, userDoc);
+        },
+        function (userDoc, callback) {
+            cryptoHash.compare(userPassword, userDoc.password)
+                .then(function (comparisonResult) {
+                    callback(null, comparisonResult, userDoc);
+                }, function (error) {
+                    callback(new Error(errorMessage));
+                });
+        },
+        function (comparisonResult, userDoc, callback) {
+            if (!comparisonResult) {
+                callback(new Error(errorMessage));
+            }
+            
+            callback(null, userDoc);
+        }
+    ], function (error, userDoc) {
+        if (error) {
+            return apiView(res, {
                 status: 401,
                 message: error.message
             });
+        }
+        
+        var userInfo = userDoc.toObject(), 
+            tokenPayload = lodash.pick(userInfo, ['_id', 
+                                                  'username', 
+                                                  'permissions', 
+                                                  'email']);
+        
+        apiView(res, {
+            status: 200,
+            message: "Succesfully authenticated!",
+            data: lodash.merge({
+                authToken: authToken.generate(tokenPayload)
+            }, userInfo)
         });
+    });
 
 }
 
